e8f608ec6b2a835cf9d92ac84d03a8776523cd82
yongbin1 <rhdydqls1024@naver.com>
2023-06-14 23:07:47 +0900

    :memo: :: 2023.06.14 Python level0 프로그래머스 코딩테스트 연습

--- codingTest/level0/code5.py
+++ codingTest/level0/code5.py
@@ -1,7 +1,6 @@
 def solution(my_string, overwrite_string, s):
     answer = ''
-    print(my_string[s:len(overwrite_string)])
-    answer = my_string.replace(my_string[s:len(overwrite_string)], overwrite_string)
+    answer = my_string.replace(my_string[s:len(overwrite_string) + s], overwrite_string)
     return answer
 
 print(solution("He11oWor1d", "lloWorl", 2))
