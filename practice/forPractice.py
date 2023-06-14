2c7f0761e3b1bfe11c15bde846b5c2eebb1da174
yongbin1 <rhdydqls1024@naver.com>
2023-06-14 23:06:21 +0900

    :memo: :: 2023.06.14 Python 공부 내용

---
+++ practice/forPractice.py
@@ -0,0 +1,8 @@
+# for문
+
+for waiting_no in range(1, 6): # 1, 2, 3, 4, 5
+    print("대기 번호 : {0}".format(waiting_no))
+
+starbucks = ["아이언맨", "토르", "그루트"]
+for customer in starbucks:
+    print("{0}, 커피가 준비되었습니다.".format(customer))
