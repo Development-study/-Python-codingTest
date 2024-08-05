5fa068646916299b1a9537302970241e9e9d2077
yongbin1 <rhdydqls1024@naver.com>
2024-08-05 10:03:08 +0900

    Python Baekjoon 1000, 1003 풀이

--- codingTest/baekjoon/code1003.py
+++ codingTest/baekjoon/code1003.py
@@ -1,30 +1,41 @@
 # 피보나치 함수 (백준 1003번 문제)
 
-import sys
-input = sys.stdin.readline
+# 다음 소스는 N번째 피보나치 수를 구하는 C++ 함수이다.
 
+# int fibonacci(int n) {
+#     if (n == 0) {
+#         printf("0");
+#         return 0;
+#     } else if (n == 1) {
+#         printf("1");
+#         return 1;
+#     } else {
+#         return fibonacci(n‐1) + fibonacci(n‐2);
+#     }
+# }
+# fibonacci(3)을 호출하면 다음과 같은 일이 일어난다.
 
-def fibonacci(n):
-    global z, o
-    if n == 0:
-        z += 1
-        return 0
-    elif n == 1:
-        o += 1
-        return 1
-    else:
-        return fibonacci(n - 1) + fibonacci(n - 2)
+# fibonacci(3)은 fibonacci(2)와 fibonacci(1) (첫 번째 호출)을 호출한다.
+# fibonacci(2)는 fibonacci(1) (두 번째 호출)과 fibonacci(0)을 호출한다.
+# 두 번째 호출한 fibonacci(1)은 1을 출력하고 1을 리턴한다.
+# fibonacci(0)은 0을 출력하고, 0을 리턴한다.
+# fibonacci(2)는 fibonacci(1)과 fibonacci(0)의 결과를 얻고, 1을 리턴한다.
+# 첫 번째 호출한 fibonacci(1)은 1을 출력하고, 1을 리턴한다.
+# fibonacci(3)은 fibonacci(2)와 fibonacci(1)의 결과를 얻고, 2를 리턴한다.
+# 1은 2번 출력되고, 0은 1번 출력된다. N이 주어졌을 때, fibonacci(N)을 호출했을 때, 0과 1이 각각 몇 번 출력되는지 구하는 프로그램을 작성하시오.
+
+zero = [1, 0, 1]
+one = [0, 1, 1]
+
+def fibonacci(num):
+    length = len(zero)
+    if num >= length:
+        for i in range(length, num+1):
+            zero.append(zero[i-1] + zero[i-2])
+            one.append(one[i-1] + one[i-2])
+    print('{} {}'.format(zero[num], one[num]))
+
+T = int(input())
     
-def solution():
-    global z, o
-    t = int(input())
-    for i in range(t):
-        n = int(input())
-        fibonacci(n)
-        print("{0} {1}".format(z, o))
-        z = 0
-        o = 0
-
-z = 0
-o = 0
-solution()
+for _ in range(T):
+    fibonacci(int(input()))
